b37f5c19b84e7c960359ddd698c000337ae80c68
ldc19951103 <58405592+ldc19951103@users.noreply.github.com>
2019-12-02 09:41:40 +0900

    12.02C#授業

--- Class06/Class06/Program.cs
+++ Class06/Class06/Program.cs
@@ -10,8 +10,8 @@ namespace Class06
 	{
 		static void Main(string[] args)
 		{
-			
-
+            
+            Console.WriteLine("li ding cheng");
 			Console.WriteLine("press return to quit.");
 			Console.ReadLine();
 		}

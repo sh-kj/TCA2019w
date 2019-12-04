bd20a265d27975880e632e17425555f25dae9e8c
Hirayamain <57649831+Hirayamain@users.noreply.github.com>
2019-12-04 11:48:18 +0900

    おくてぃ討伐（）

--- Class06/Class06/Program.cs
+++ Class06/Class06/Program.cs
@@ -10,9 +10,33 @@ namespace Class06
 	{
 		static void Main(string[] args)
 		{
-			
+            Console.WriteLine("GameStart");
 
-			Console.WriteLine("press return to quit.okyano");
+            Player player = new Player("勇者オキャノーン", 1, 0, 20, 10, 5);
+
+            while(true)
+            {
+                Enemy enemy = new Enemy("okuty", 10, 8, 2, 5);
+                Console.WriteLine(enemy.Name + "が現れた！");
+
+                //battle
+                Battle battle = new Battle(player, enemy);
+                bool battleIsEnd = false;
+                while(!battleIsEnd)
+                {
+                    Console.WriteLine("コマンド？");
+                    Console.ReadLine();
+
+                    battleIsEnd = battle.AdvanceTurn();
+                  
+
+                    Console.WriteLine(player.Name + "のHP：" + player.HP);
+                }
+            }
+
+            Console.WriteLine("ゲームオーバー");
+
+		
 			Console.ReadLine();
 		}
 	}

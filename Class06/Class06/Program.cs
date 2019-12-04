2247f7236c20a1a27cee93a505885db087b8fc85
tcags2019.member <tcags2019.member@gmail.com>
2019-12-04 11:58:32 +0900

    変更テスト

--- Class06/Class06/Program.cs
+++ Class06/Class06/Program.cs
@@ -10,8 +10,29 @@ namespace Class06
 	{
 		static void Main(string[] args)
 		{
-			
+			Console.WriteLine("game start");
 
+			Player player = new Player("ザコー", 1, 0, 20, 10, 5);
+
+			while(true)
+			{
+				Enemy enemy = new Enemy("スライム", 10, 8, 2, 5);
+
+				Console.WriteLine(enemy.Name + "が現れた!");
+
+				battle battle = new battle(player,enemy);
+
+				bool battleIsEnd = false;
+				while (!battleIsEnd)
+				{
+					Console.WriteLine("コマンド?");
+					Console.ReadLine();
+
+					battleIsEnd = battle.AdvanceTurn();
+
+					Console.WriteLine(player.Name + "のHP" + player.HP);
+				}
+			}
 			Console.WriteLine("press return to quit.");
 			Console.ReadLine();
 		}
---
+++ Class06/Class06/battle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class06
+{
+	class battle
+	{
+		public Player BattlePlayer {
+			get;private set;
+		}
+
+		public Enemy BattleEnemy {
+			get; private set;
+		}
+
+		public battle(Player player, Enemy enemy) {
+			BattlePlayer = player;
+			BattleEnemy = enemy;
+		}
+		public bool AdvanceTurn()
+		{
+			int damege = BattlePlayer.Attack(BattleEnemy);
+			Console.WriteLine(BattlePlayer.Name + "の攻撃!" + BattleEnemy + "に" + damege + "のダメージ");
+
+			if( !BattleEnemy.IsAlive)
+			{
+				Console.WriteLine(BattleEnemy.Name + "を倒した!");
+				return true;
+			}
+
+			damege = BattleEnemy.Attack(BattlePlayer);
+			Console.WriteLine(BattleEnemy.Name + "の攻撃!" + BattlePlayer + "に" + damege + "のダメージ");
+
+			if (!BattlePlayer.IsAlive)
+			{
+				Console.WriteLine(BattlePlayer.Name + "は倒れた...");
+				return true;
+			}
+			return false;
+		}
+
+	}
+}

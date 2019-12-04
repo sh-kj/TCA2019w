f4f1780447b2f208cdcf642b10067811003d3c4b
あまおう <Amaou0014@gmail.com>
2019-12-04 11:46:20 +0900

    おきゃのがokutyを殴る

--- Class06/Class06/Program.cs
+++ Class06/Class06/Program.cs
@@ -10,10 +10,33 @@ namespace Class06
 	{
 		static void Main(string[] args)
 		{
-			
+			Console.WriteLine("ゲームスタート");
 
-			Console.WriteLine("press return to quit.");
+			Player player = new Player("おきゃの", 1, 0, 20, 10, 5);
+
+			while (true)
+			{
+				Enemy enemy = new Enemy("okuty", 10, 8, 2, 5);
+				Console.WriteLine(enemy.Name + "が現れた！");
+
+				Battle battle = new Battle(player,enemy);
+
+				bool battleIsEnd = false;
+				while (!battleIsEnd)
+				{
+					Console.WriteLine("コマンド？");
+					Console.ReadLine( );
+
+					battleIsEnd = battle.AdvanceTurn();
+
+					Console.WriteLine(player.Name + "のHP：" + player.HP);
+				}
+
+			}
+			Console.WriteLine("ゲームオーバー");
 			Console.ReadLine();
 		}
+
+
 	}
 }

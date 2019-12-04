c8b150703fe15c2fbbbe903f10b62c4174643a9f
kuroemon1239 <58493299+kuroemon1239@users.noreply.github.com>
2019-12-04 12:01:48 +0900

    damage caculate

--- Class06/Class06/Program.cs
+++ Class06/Class06/Program.cs
@@ -4,16 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Class06
-{
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			
+namespace Class06 {
+	class Program {
+		static void Main( string[ ] args ) {
 
-			Console.WriteLine("press return to quit.");
-			Console.ReadLine();
+			Console.WriteLine( "game start" );
+
+
+			Player player = new Player( "hero", 1, 0, 20, 10, 5 );
+
+			while (player.IsAlive ) {
+				Enemy enemy = new Enemy( "slime", 10, 8, 2, 5 );
+				Console.WriteLine( enemy.Name + "discover!" );
+				//battle
+				Battle battle = new Battle( player, enemy );
+				bool battleIsEnd = false;
+				while ( !battleIsEnd ) {
+					Console.WriteLine( "comment?" );
+					Console.ReadLine( );
+
+					battleIsEnd = battle.AdvanceTurn( );
+
+					Console.WriteLine( player.Name + "'s hp:" + player.HP );
+				}
+			}
+
+			Console.WriteLine( "game over" );
+			Console.WriteLine( "press return to quit" );
 		}
 	}
 }
+
+
---
+++ Class06/Class06/battle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class06 
+	{
+	class Battle 
+	{
+		
+		
+				public Player BattlePlayer { get; private set; }
+
+				public Enemy BattleEnemy { get; private set; }
+				
+				public Battle(Player player, Enemy enemy) 
+			
+				{
+				BattlePlayer = player;
+				BattleEnemy = enemy;
+				}
+				public bool AdvanceTurn() 
+				{
+				//player attack enemy
+				int damage = BattlePlayer.Attack( BattleEnemy );
+				Console.WriteLine( BattlePlayer.Name + "attack!" + BattleEnemy.Name + "to" + damage + "damage" );
+				//enemy check live or not?
+				if(!BattleEnemy.IsAlive) 
+				{
+				//dead
+				Console.WriteLine( BattleEnemy.Name + "was defeated!" );
+				return true;
+				}
+				//enemy attack player
+				damage = BattleEnemy.Attack( BattlePlayer );
+				Console.WriteLine( BattleEnemy.Name + "attack!" +BattlePlayer.Name+"to"+damage+"damage!");
+				//player check dead or not
+				if(!BattlePlayer.IsAlive) 
+				{
+				Console.WriteLine( BattlePlayer.Name + "was killed by enenmy" );
+				return true;
+				}
+				return false;
+				}
+
+			}
+	
+
+	}
+	
+
+

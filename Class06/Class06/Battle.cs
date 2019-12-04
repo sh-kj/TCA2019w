d4706f910c8c95c72991d7d888830e796298cfa0
sunyongxin1 <58493389+sunyongxin1@users.noreply.github.com>
2019-12-04 11:58:33 +0900

    ゲームループ・ダメーじ

---
+++ Class06/Class06/Battle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class06
+{
+	class Battle
+	{
+		public Player BattlePlayer
+			{ get;private set; }
+		public Enemy BattleEnemy 
+			{ get; private set; }
+		public Battle (Player player,Enemy enemy)
+		{
+			BattlePlayer = player;
+			BattleEnemy = enemy;
+			
+		}
+		public bool AdvanceTurn()
+		{
+			int damage = BattlePlayer.Attack(BattleEnemy);
+			Console.WriteLine(BattlePlayer.Name + "attack"+
+			BattleEnemy.Name + "ni" + damage+"shagnhai");
+
+			if (!BattleEnemy.IsAlive)
+			{
+				Console.WriteLine(BattleEnemy.Name + "ss");
+				return true;
+
+			}
+
+			 damage = BattleEnemy.Attack(BattlePlayer);
+			Console.WriteLine(BattleEnemy.Name + "attack" +
+			BattlePlayer.Name + "ni" + damage + "shagnhai");
+			if (!BattleEnemy.IsAlive)
+			{
+				Console.WriteLine(BattlePlayer.Name + "ss");
+				return true;
+
+			}
+			return false;
+		}
+	}
+}
--- Class06/Class06/Program.cs
+++ Class06/Class06/Program.cs
@@ -11,7 +11,28 @@ namespace Class06
 		static void Main(string[] args)
 		{
 
-			Console.WriteLine("ssss");
+
+			Console.WriteLine("gamestart");
+			Player player = new Player("youxia", 1, 0, 20, 10, 5);
+			while (true)
+			{
+				Enemy enemy = new Enemy("sraim", 10, 8, 2, 5);
+				Console.WriteLine(enemy.Name + "gareta");
+
+				Battle battle = new Battle(player, enemy);
+				bool battleIsEnd = false;
+				while (true)
+				{
+					Console.WriteLine("komanto");
+					Console.ReadLine();
+
+					battleIsEnd = battle.AdvanceTurn();
+
+					battle.AdvanceTurn();
+					Console.WriteLine(player.Name + "deHP" + player.HP);
+				}
+			}
+			Console.WriteLine(player.Name + "deHP" + player.HP);
 			Console.WriteLine("press return to quit.");
 			Console.ReadLine();
 		}

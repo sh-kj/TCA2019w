9b19ed8e12d8a73473e31ff5a68b8fc10bddb65c
リンシン <57927468+flaquf1222@users.noreply.github.com>
2019-12-04 11:57:42 +0900

    20191204

--- Class06/Class06/Character.cs
+++ Class06/Class06/Character.cs
@@ -91,10 +91,24 @@ namespace Class06
 	//ダメージ計算用クラス
 	static class DamageCalculator
 	{
-
+		private static Random RandomCaculator = new Random( DateTime.Now.Millisecond );
 		public static int CalcDamage(Character attacker, Character target)
 		{
-			return attacker.AttackPower - target.DefencePower;
+			//return attacker.AttackPower - target.DefencePower;
+			int minDamage = (attacker.AttackPower - target.DefencePower / 2) / 4;
+			int maxDamage = (attacker.AttackPower - target.DefencePower / 2) / 2;
+
+			if (minDamage < 1)
+			{
+				minDamage = 1;
+			}
+			if (maxDamage < 1)
+			{
+				maxDamage = 1;
+			}
+			int damage = RandomCaculator.Next(minDamage, maxDamage);
+
+			return damage;
 		}
 	}
 

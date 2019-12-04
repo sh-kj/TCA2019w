c68e08c0145636d5f48040afad095e2d06a1c9b0
1302ayaka <57651826+1302ayaka@users.noreply.github.com>
2019-12-04 12:01:06 +0900

    ゲームループ実装、ダメージ計算改善

---
+++ Class06/Class06/Battle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class06
+{
+    class Battle
+    {
+        public Player BattlePlayer 
+            { get; private set; }
+
+        public Enemy BattleEnemy 
+            { get; private set; }
+
+        public Battle(Player player, Enemy enemy)
+        {
+            BattlePlayer = player;
+            BattleEnemy = enemy;
+        }
+
+        public bool AdvanceTurn(/*コマンド内容*/)
+        {
+            //プレイヤーから敵への攻撃
+            int damage = BattlePlayer.Attack(BattleEnemy);
+            Console.WriteLine(BattlePlayer.Name + "の攻撃" + BattleEnemy.Name + "に" + damage + "のダメージ！");
+
+            //敵の生存チェック
+            if(!BattleEnemy.IsAlive)
+            {
+                //敵が死んだ場合
+                Console.WriteLine(BattleEnemy.Name + "を倒した！！");
+                return true;
+            }
+
+            //敵からプレイヤーへの攻撃
+            damage = BattleEnemy.Attack(BattlePlayer);
+            Console.WriteLine(BattleEnemy.Name + "の攻撃！" + BattlePlayer.Name + "に" + damage + "のダメージ！");
+
+            //プレイヤーの生存チェック
+            if(!BattlePlayer.IsAlive)
+            {
+                Console.WriteLine(BattlePlayer.Name + "は倒れた...");
+                return true;
+            }
+
+            return false;
+        }
+
+    }
+}

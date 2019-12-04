189aa37997d71f6c5538fc27e2eb7d1c49583f13
リンシン <57927468+flaquf1222@users.noreply.github.com>
2019-12-04 11:00:41 +0900

    20191204

--- Class06/Class06/Program.cs
+++ Class06/Class06/Program.cs
@@ -10,10 +10,29 @@ namespace Class06
 	{
 		static void Main(string[] args)
 		{
+
+			Console.WriteLine("ゲームスタート");
+
+			Player player = new Player("Okuty", 1, 0, 20, 10, 5);
+
+			while( true )
+			{
+				Enemy enemy = new Enemy("tsukada", 10, 8, 2, 5);
+				Console.WriteLine(enemy.Name + "が現れた！");
+
+				Battle battle = new Battle(player, enemy);
+				while( true )
+				{
+					Console.WriteLine("コマンド？");
+					Console.ReadLine();
+
+					battle.AdvanceTurn();
+
+					Console.WriteLine(player.Name + "のHP:" + player.HP);
+				}
+			}
+
 			
-
-
-			dasgfgser
 
 			Console.WriteLine("press return to quit.");
 			Console.ReadLine();

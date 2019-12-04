6435cd663181565e4f1e2af87120687e264878de
OkadaYukumi <okada.yukumi@gmail.com>
2019-12-04 11:58:06 +0900

    戦闘ループ・戦闘計算 実装

---
+++ Class06/Class06/Battle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class06 {
+	class Battle {
+		public Player BattlePlayer {
+			get; private set;
+		}
+		public Enemy BattleEnemy {
+			get; private set;
+		}
+
+		public Battle( Player player, Enemy enemy ) {
+			BattlePlayer = player;
+			BattleEnemy = enemy;
+		}
+
+		public bool AdvaneTurn( /*コマンド内容*/ ) {
+			//プレイヤー → 敵
+			int damege = BattlePlayer.Attack( BattleEnemy );
+			Console.WriteLine( BattlePlayer.Name + "の攻撃!" +
+				BattleEnemy.Name + "に" + damege + "のダメージ!" );
+			//生存チェック
+			if ( !BattleEnemy.IsAlive ) {
+				//死んだら	
+				Console.WriteLine( BattleEnemy.Name + "を倒した。" );
+				
+				/*
+				BattlePlayer.Exp += BattleEnemy.GainExp;
+				Console.WriteLine( BattleEnemy.GainExp + "を取得した。"
+					+ "経験値が" + BattlePlayer.Level + "になった。" );
+				*/
+				
+				return true;
+			}
+
+			//敵 → プレイヤー
+			damege = BattleEnemy.Attack( BattlePlayer );
+			Console.WriteLine( BattleEnemy.Name + "の攻撃!" +
+				BattlePlayer.Name + "に" + damege + "のダメージ!" );
+			//生存チェック
+			if ( !BattlePlayer.IsAlive ) {
+				//死んだら	
+				Console.WriteLine( BattlePlayer.Name + "は倒れた..." );
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
--- Class06/Class06/Program.cs
+++ Class06/Class06/Program.cs
@@ -4,14 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Class06 {
 	class Program {
+
+		const int PLAYER_LEVEL = 0;
+		const int PLAYER_EXP = 0;
+		const int PLAYER_HP = 20;
+		const int PLAYER_ATTACK = 10;
+		const int PLAYER_DEFEND = 5;
+
+		const int SLIME_HP = 10;
+		const int SLIME_ATTACK = 8;
+		const int SLIME_DEFEND = 1;
+		const int SLIME_GAINEXP = 5;
+		
+		
 		static void Main( string[ ] args ) {
 
+			Console.WriteLine( "ゲームスタート" );
+			Player _player = new Player( "Okuty", PLAYER_LEVEL, PLAYER_EXP, PLAYER_HP, PLAYER_ATTACK, PLAYER_DEFEND );
 
-			Console.WriteLine( "Okuty." );
-			Console.WriteLine( "press return to quit." );
+			while ( _player.IsAlive ) {
+				Enemy _enemy = new Enemy( "スライム", SLIME_HP, SLIME_ATTACK, SLIME_DEFEND, SLIME_GAINEXP );
+				Console.WriteLine( _enemy.Name + "が現れた!" );
+
+				//バトル処理
+				Battle battle = new Battle( _player, _enemy );
+				bool _battles_end = false;
+				while ( !_battles_end ) {
+					Console.WriteLine( "コマンド？" );
+					Console.ReadLine( );
+
+					_battles_end = battle.AdvaneTurn( );
+
+					if ( !_battles_end ) {
+						Console.WriteLine( _player.Name + "のHP" + _player.HP );
+					}
+				}
+
+			}
+
+
+
+
+			Console.WriteLine( "\nゲームオーバー" );
 			Console.ReadLine( );
+
 		}
 	}
 }

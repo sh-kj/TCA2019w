98e172477b313bb1b4a681e75a5cd63c694bbf64
OkadaYukumi <okada.yukumi@gmail.com>
2019-12-04 10:13:12 +0900

    Add Draw Okuty

--- Class06/Class06/Program.cs
+++ Class06/Class06/Program.cs
@@ -4,16 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Class06
-{
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			
+namespace Class06 {
+	class Program {
+		static void Main( string[ ] args ) {
 
-			Console.WriteLine("press return to quit.");
-			Console.ReadLine();
+
+			Console.WriteLine( "Okuty." );
+			Console.WriteLine( "press return to quit." );
+			Console.ReadLine( );
 		}
 	}
 }

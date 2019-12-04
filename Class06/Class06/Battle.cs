5b1369ba7956021a9993bddf04c131cdf089bd56
zirigaozhao <zirigaozhao@yahoo.co.jp>
2019-12-04 11:00:45 +0900

    2019/12/04

---
+++ Class06/Class06/Battle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class06
+{
+	class Battle
+	{
+		public Player BattlePlayer 
+			{ get; private set; }
+
+		public Enemy BattleEnemy 
+		{ get; private set; }
+
+		public Battle(Player player,Enemy enemy)
+		{
+			BattlePlayer = player;
+			BattleEnemy = enemy;
+		}
+
+		public void AdvanceTurn()
+		{
+
+			int damage=BattlePlayer.Attack(BattleEnemy);
+
+			Console.WriteLine(BattlePlayer.Name + "の攻撃！" + BattleEnemy.Name + "に" + damage + "のダメージ!");
+
+			damage = BattleEnemy.Attack(BattlePlayer);
+			Console.WriteLine(BattleEnemy.Name + "の攻撃！" + BattlePlayer.Name + "に" + damage + "のダメージ!");
+
+		}
+	}
+}
--- Class06/Class06/Program.cs
+++ Class06/Class06/Program.cs
@@ -10,12 +10,27 @@ namespace Class06
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("ゲームスタート");
 
+			Player player = new Player("勇者", 1, 0, 20, 10, 5);
+			while (true)
+			{
+				Enemy enemy = new Enemy("スライム", 10, 8, 2, 5);
+				Console.WriteLine(enemy.Name + "が現れた！");
+				//バトル
 
+				Battle battle = new Battle(player, enemy);
 
+				while (true)
+				{
+					Console.WriteLine("コマンド？");
+					Console.ReadLine();
 
-			jklasdfjklsdfjklsdfajkl;asdfjkl;sdf
-			
+					battle.AdvanceTurn();
+					Console.WriteLine(player.Name + "のHP:" + player.HP);
+				}
+
+			}
 
 			Console.WriteLine("press return to quit.");
 			Console.ReadLine();

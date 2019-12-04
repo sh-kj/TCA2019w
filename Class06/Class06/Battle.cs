340301fa8f79452003f19ee8ba79bb4046cecf9a
BT-7274 <57926649+yatomo0225@users.noreply.github.com>
2019-12-04 12:01:43 +0900

    今日の分

---
+++ Class06/Class06/Battle.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class06
+{
+    class Battle
+    {
+        public Player BattlePlayer
+        { get; private set;  }
+
+        public Enemy BattleEnemy
+        { get; private set; }
+
+        public Battle(Player player, Enemy enamy)
+        {
+            BattlePlayer = player;
+            BattleEnemy = enamy;
+        }
+        public bool AdvanceTurn(/*コマンド内容*/)
+        {
+            int damage = BattlePlayer.Attack(BattleEnemy);
+
+            Console.WriteLine(BattlePlayer.Name + "の攻撃!" + BattleEnemy.Name + "に" + damage + "のダメージ!");
+
+            if (!BattleEnemy.IsAlive)//敵の生存チェック
+            {
+                Console.WriteLine(BattlePlayer.Name + "は倒れた");
+                return true;
+            }
+
+
+            damage = BattleEnemy.Attack(BattlePlayer);
+            Console.WriteLine(BattleEnemy.Name + "に" + damage + "のダメージ!");
+            //プレイヤーの生存チェック
+            if(!BattlePlayer.IsAlive)
+            {
+                Console.WriteLine(BattlePlayer.Name + "は倒れた");
+                return true;
+            }
+
+
+ 
+            return false;
+        }
+
+      
+
+    }
+}
--- Class06/Class06/Character.cs
+++ Class06/Class06/Character.cs
@@ -92,9 +92,25 @@ namespace Class06
 	static class DamageCalculator
 	{
 
-		public static int CalcDamage(Character attacker, Character target)
+        private static Random RandomCalculator = new Random(DateTime.Now.Millisecond);
+
+
+        public static int CalcDamage(Character attacker, Character target)
 		{
-			return attacker.AttackPower - target.DefencePower;
+
+            //最低ダメージ=(勇者の攻撃力-敵の守備力+2)+4
+            //最高ダメージ=(勇者の攻撃力-敵の守備力+2)+2
+            int minDamage = (attacker.AttackPower - target.DefencePower / 2) / 4;
+            int maxDamage = (attacker.AttackPower - target.DefencePower / 2) / 2;
+
+            if (minDamage < 1)
+                minDamage = 1;
+            if (maxDamage < 1)
+                maxDamage = 1;
+                int damage = RandomCalculator.Next(minDamage, maxDamage);
+
+            return damage;
+
 		}
 	}
 

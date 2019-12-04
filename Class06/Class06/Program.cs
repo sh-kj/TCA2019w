8c6fc2a98b688314317bab2f902d1172b2b5d8c5
Unno-Ryo <grisiasachi@gmail.com>
2019-12-04 11:57:38 +0900

    ゲームループ実装、ダメージ計算実装

--- Class06/Class06/Program.cs
+++ Class06/Class06/Program.cs
@@ -10,13 +10,33 @@ namespace Class06
 	{
 		static void Main(string[] args)
 		{
-			
+			Console.WriteLine("ゲームスタート");
+			Player _Daisuke = new Player("大介", 1, 0, 20, 10, 5);
+			Player _okuty = new Player("おくてぃ～", 1, 0, 1, 1, 1);
+
+			while (_Daisuke.IsAlive)
+			{
+				Enemy enemy = new Enemy("プランナー", 10, 10, 5, 15);
+				Console.WriteLine(enemy.Name + "が現れた！");
+
+				Battle battle = new Battle( _Daisuke
+					, enemy);
+				bool battleisEnd = false;
+				while (!battleisEnd)
+				{
+
+					Console.WriteLine("コマンド？");
+					Console.ReadLine();
+
+					battleisEnd = battle.AdvanceTrum();
+
+					Console.WriteLine(_Daisuke.Name + "のHP：" + _Daisuke.HP);
 
 
+				}
 
-
-			Console.WriteLine("press return to quit.");
-			Console.ReadLine();
+				Console.ReadLine();
+			}
 		}
 	}
 }

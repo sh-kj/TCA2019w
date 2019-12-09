7eee4736a39ef98729099fff12579d2b819cd119
radiants <shiino.kj@gmail.com>
2019-12-09 11:40:03 +0900

    敵マスターデータの生成/読み出し、敵のランダム選択

--- Class06/Class06/Program.cs
+++ Class06/Class06/Program.cs
@@ -10,12 +10,40 @@ namespace Class06
 	{
 		static void Main(string[] args)
 		{
+			EnemyMaster enemyMaster;
+			if ( System.IO.File.Exists( @"D:\enemy.json" ) )
+			{
+				//マスターデータファイルが存在する
+				string json = System.IO.File.ReadAllText( @"D:\enemy.json" );
+				enemyMaster = Newtonsoft.Json.JsonConvert.DeserializeObject<EnemyMaster>( json );
+			}
+			else
+			{
+				//存在しない場合、デフォルトの値で作ってjsonを出力
+				enemyMaster = new EnemyMaster( );
+				enemyMaster.Parameters = new List<EnemyParameter>( );
+
+				EnemyParameter enemyParameter = new EnemyParameter( );
+				enemyParameter.Name = "メタルスライム";
+				enemyParameter.MaxHP = 5;
+				enemyParameter.AttackPower = 10;
+				enemyParameter.DefencePower = 300;
+				enemyParameter.GainExp = 2000;
+
+				enemyMaster.Parameters.Add( enemyParameter );
+
+				string result = Newtonsoft.Json.JsonConvert.SerializeObject( enemyMaster );
+				System.IO.File.WriteAllText( @"D:\enemy.json", result );
+			}
+
 			Player player = new Player( "勇者", 1, 0, 20, 12, 5 );
 
 			while ( true )
 			{
 				//エンカウント
-				Enemy enemy = new Enemy( "スライム", 7, 10, 3, 5 );
+				int enemyIndex = DamageCalculator.RandomProvider.Next( enemyMaster.Parameters.Count );
+				Enemy enemy = new Enemy( enemyMaster.Parameters[ enemyIndex ] );
+
 				Battle battle = new Battle( player, enemy );
 
 				Console.WriteLine( enemy.Name + "が現れた！" );

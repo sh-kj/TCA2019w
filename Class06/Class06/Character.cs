7aab6f813043bec41465f9b01c7b14d5f11423c6
tcags2019.member <tcags2019.member@gmail.com>
2019-12-09 11:57:25 +0900

    エネミーランダム、簡易化

--- Class06/Class06/Character.cs
+++ Class06/Class06/Character.cs
@@ -53,8 +53,15 @@ namespace Class06
 			this.DefencePower = defencePower;
 			this.GainExp = gainExp;
 		}
+		public Enemy(EnemyParameter parameter ) {
+			this.Name = parameter.Name;
+			this.MaxHP =parameter.MaxHP;
+			this.HP = parameter.MaxHP;
+			this.AttackPower = parameter.AttackPower;
+			this.DefencePower = parameter.DefencePower;
+			this.GainExp = parameter.GainExp;
+		}
 	}
-
 	class Player : Character
 	{
 		public int Level
@@ -91,7 +98,7 @@ namespace Class06
 	//ダメージ計算用クラス
 	static class DamageCalculator
 	{
-		private static Random RandomProvider = new Random(DateTime.Now.Millisecond);
+		public static Random RandomProvider = new Random(DateTime.Now.Millisecond);
 		public static int CalcDamage(Character attacker, Character target)
 		{
 			//最低ダメージ＝（勇者の攻撃力 - 敵の守備力÷2）÷4
@@ -99,14 +106,16 @@ namespace Class06
 			int minmumDamege = (attacker.AttackPower - target.DefencePower / 2) / 4;
 			int maximumDamage = (attacker.AttackPower - target.DefencePower / 2) / 2;
 
-			if (minmumDamege < 1)
+			if ( minmumDamege < 1 ) {
 				minmumDamege = 1;
-			if (maximumDamage < 1)
+			}
+			if ( maximumDamage < 1 ) {
 				maximumDamage = 1;
+			}
 
 			int damege = RandomProvider.Next(minmumDamege, maximumDamage);
 
-			return attacker.AttackPower - target.DefencePower;
+			return damege;
 		}
 	}
 

75515444ba9b2aee7c8c04f5526c5286ee85ad00
1302ayaka <57651826+1302ayaka@users.noreply.github.com>
2019-12-11 11:26:33 +0900

    モンスターのランダム出現

--- Class06/Class06/Character.cs
+++ Class06/Class06/Character.cs
@@ -53,6 +53,15 @@ namespace Class06
 			this.DefencePower = defencePower;
 			this.GainExp = gainExp;
 		}
+
+        public Enemy( EnemyParameter parameter )
+        {
+            this.Name = parameter.Name;
+            this.MaxHP = parameter.MaxHP;
+            this.AttackPower = parameter.AttackPower;
+            this.DefencePower = parameter.DefencePower;
+            this.GainExp = parameter.GainExp;
+        }
 	}
 
 	class Player : Character
@@ -91,7 +100,7 @@ namespace Class06
 	//ダメージ計算用クラス
 	static class DamageCalculator
 	{
-		private static Random RandomCalculator = new Random(DateTime.Now.Millisecond);
+		public static Random RandomCalculator = new Random(DateTime.Now.Millisecond);
 
 		public static int CalcDamage(Character attacker, Character target)
 		{

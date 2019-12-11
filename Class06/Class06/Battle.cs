3f84c0ab3a51a7c2b01cac9b861be4018540efb4
まひろ <mahimahi20000928@gmail.com>
2019-12-11 12:11:00 +0900

    パーティを複数に

--- Class06/Class06/Battle.cs
+++ Class06/Class06/Battle.cs
@@ -11,20 +11,26 @@ namespace Class06
 		public Player BattlePlayer
 			{ get; private set; }
 
+		public Player BattlePlayer2
+			{ get; private set; }
+
 		public Enemy BattleEnemy
 			{ get; private set; }
 
-		public Battle(Player player, Enemy enemy)
+		public Battle(Player player1, Player player2, Enemy enemy)
 		{
-			BattlePlayer = player;
+			BattlePlayer = player1;
+			BattlePlayer2 = player2;
 			BattleEnemy = enemy;
 		}
 
 		public bool AdvanceTrum(/*コマンド内容*/)
 		{
 			int damage = BattlePlayer.Attack(BattleEnemy);
-			Console.WriteLine(BattlePlayer.Name + "の攻撃",
-			BattleEnemy.Name + "に" + damage + "のダメージ");
+			int damage2 = BattlePlayer2.Attack(BattleEnemy);
+
+			Console.WriteLine(BattlePlayer.Name + "の攻撃" + BattleEnemy.Name + "に" + damage + "のダメージ");
+			Console.WriteLine(BattlePlayer2.Name + "の攻撃" + BattleEnemy.Name + "に" + damage2 + "のダメージ");
 
 			if (!BattleEnemy.IsAlive)
 			{
@@ -32,9 +38,17 @@ namespace Class06
 				return true;
 			}
 
+			int random = DamageCalculator.RandomCalculator.Next(10);
+
+			if(random % 2 == 0){
 			damage = BattleEnemy.Attack(BattlePlayer);
-			Console.WriteLine(BattleEnemy.Name + "の攻撃",
-			BattlePlayer.Name + "に" + damage + "のダメージ");
+			Console.WriteLine(BattleEnemy.Name + "の攻撃！" +
+			BattlePlayer.Name + "に" + damage + "のダメージ！");
+			}else{
+				damage = BattleEnemy.Attack(BattlePlayer2);
+			Console.WriteLine(BattleEnemy.Name + "の攻撃！" +
+			BattlePlayer2.Name + "に" + damage + "のダメージ！");
+			}
 
 			if (!BattlePlayer.IsAlive)
 			{
--- Class06/Class06/Program.cs
+++ Class06/Class06/Program.cs
@@ -10,17 +10,64 @@ namespace Class06
 	{
 		static void Main(string[] args)
 		{
+
+			EnemyMaster enemyMaster = null;
+			try
+			{
+				string json = System.IO.File.ReadAllText(@"D:\enemy.json");
+				enemyMaster = Newtonsoft.Json.JsonConvert.DeserializeObject<EnemyMaster>(json);
+			}
+
+			catch (Exception e)
+			{
+				Console.WriteLine("マスターデータの読み込みに失敗しました。" + e.ToString());
+			}
+
+			if (enemyMaster == null)
+			{
+				//マスターデータの読み込みに失敗しているのでデフォルト値でマスターデータを作る
+				EnemyParameter parameter1 = new EnemyParameter();
+				parameter1.Name = "スライム";
+				parameter1.MaxHP = 10;
+				parameter1.AttackPower = 8;
+				parameter1.DefencePower = 2;
+				parameter1.GainXP = 5;
+
+
+				EnemyParameter parameter2 = new EnemyParameter();
+				parameter2.Name = "プランナー";
+				parameter2.MaxHP = 1;
+				parameter2.AttackPower = 20;
+				parameter2.DefencePower = 5;
+				parameter2.GainXP = 100;
+
+				EnemyMaster master = new EnemyMaster();
+				master.Parameters = new List<EnemyParameter>();
+				master.Parameters.Add(parameter1);
+				master.Parameters.Add(parameter2);
+
+				string json = Newtonsoft.Json.JsonConvert.SerializeObject(master);
+				System.IO.File.WriteAllText(@"D:\enemy.json", json);
+			}
+
+			
+
+
+
+
 			Console.WriteLine("ゲームスタート");
 			Player _Daisuke = new Player("大介", 1, 0, 20, 10, 5);
 			Player _okuty = new Player("おくてぃ～", 1, 0, 1, 1, 1);
 
 			while (_Daisuke.IsAlive)
 			{
-				Enemy enemy = new Enemy("プランナー", 10, 10, 5, 15);
+				int randomIndex = DamageCalculator.RandomCalculator.Next(enemyMaster.Parameters.Count);
+
+				Enemy enemy = new Enemy(enemyMaster.Parameters[randomIndex]);
 				Console.WriteLine(enemy.Name + "が現れた！");
 
 				Battle battle = new Battle( _Daisuke
-					, enemy);
+					, _okuty, enemy);
 				bool battleisEnd = false;
 				while (!battleisEnd)
 				{
@@ -37,6 +84,7 @@ namespace Class06
 
 				Console.ReadLine();
 			}
+				
 		}
 	}
 }

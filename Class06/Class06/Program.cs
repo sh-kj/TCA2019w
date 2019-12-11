6fbd76ff26538a9188a16a31b18c6b6cf7903263
1302ayaka <57651826+1302ayaka@users.noreply.github.com>
2019-12-11 11:31:02 +0900

    Revert "モンスターのランダム出現"
    This reverts commit 75515444ba9b2aee7c8c04f5526c5286ee85ad00.

--- Class06/Class06/Program.cs
+++ Class06/Class06/Program.cs
@@ -10,56 +10,6 @@ namespace Class06
 	{
 		static void Main(string[] args)
 		{
-            EnemyMaster enemyMaster = null;
-            try
-            {
-                string json = System.IO.File.ReadAllText(@"D:\enemy.json");
-
-                enemyMaster =
-                Newtonsoft.Json.JsonConvert.DeserializeObject<EnemyMaster>(json);
-            }
-            catch( Exception e)
-            {
-                Console.WriteLine("マスターデータ読み込みに失敗しました:" + e.ToString());
-            }
-
-            if( enemyMaster == null)
-            {
-                //マスターデータ読み出しに失敗しているので
-                //デフォルト値でマスターデータを作る
-                EnemyParameter parameter1 = new EnemyParameter();
-                parameter1.Name = "スライム";
-                parameter1.MaxHP = 10;
-                parameter1.AttackPower = 8;
-                parameter1.DefencePower = 2;
-                parameter1.GainExp = 5;
-
-
-                EnemyParameter parameter2 = new EnemyParameter();
-                parameter2.Name = "おくてぃー";
-                parameter2.MaxHP = 1;
-                parameter2.AttackPower = 20;
-                parameter2.DefencePower = 5;
-                parameter2.GainExp = 100;
-
-                EnemyMaster master = new EnemyMaster();
-                master.Parameters = new List<EnemyParameter>();
-                master.Parameters.Add(parameter1);
-                master.Parameters.Add(parameter2);
-
-                string json = Newtonsoft.Json.JsonConvert.SerializeObject(master);
-
-                Console.WriteLine(json);
-                System.IO.File.WriteAllText(@"D:\enemy.json", json);
-            }
-
-
-            //Console.WriteLine(enemyMaster.Parameters[2].Name + "\nMaxHP:" + enemyMaster.Parameters[0].MaxHP +
-            //    "\nAttackPower:" + enemyMaster.Parameters[2].AttackPower +
-            //     "\nDefencePower:" + enemyMaster.Parameters[2].DefencePower +
-            //      "\nGainExp:" + enemyMaster.Parameters[2].GainExp);
-
-
 
 			Console.WriteLine("ゲームスタート");
 
@@ -67,10 +17,7 @@ namespace Class06
 
 			while(player.IsAlive)
 			{
-                int rundomIndex = DamageCalculator.RandomCalculator.Next(enemyMaster.Parameters.Count);
-
-                Enemy enemy = new Enemy(enemyMaster.Parameters[rundomIndex]);
-                 
+				Enemy enemy = new Enemy("スライム", 10, 8, 2, 5);
 				Console.WriteLine(enemy.Name + "が現れた！");
 
 				//バトル

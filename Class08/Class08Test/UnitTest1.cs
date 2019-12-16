a76f57b2eaa725c74bde96d566b6463f4ad7afe5
ldc19951103 <58405592+ldc19951103@users.noreply.github.com>
2019-12-16 11:23:49 +0900

    class08-Ketut Gilang Setiadi Raka

---
+++ Class08/Class08Test/UnitTest1.cs
@@ -0,0 +1,39 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    {
+        private Class08.DamageCalculator damageCalculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            damageCalculator=new Class08.DamageCalculator();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            for(int i=0; i<10000; i++)
+            {
+                int damage = damageCalculator.Calculate(10, 5);
+                Assert.That(damage, Is.LessThan(100));
+
+            }
+        }
+
+        [Test]
+        public void test2()
+        {
+            for(int i=0; i<10000; i++)
+            {
+                int damage = damageCalculator.Calculate(1, 10);
+                Assert.That(damage, Is.GreaterThan(0));
+
+            }
+        }
+
+
+    }
+}

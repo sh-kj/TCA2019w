036868865212fdd32291fd530f5e6ae1732476f6
yomukal <57931435+yomukal@users.noreply.github.com>
2020-01-19 20:01:38 +0900

    yomukal/class6

--- TCA2019w-8bdd604563c1370a4b6e848f7213ce46e26c04b9/Class06/Class06/Program.cs
+++ TCA2019w-8bdd604563c1370a4b6e848f7213ce46e26c04b9/Class06/Class06/Program.cs
@@ -10,12 +10,65 @@ namespace Class06
 	{
 		static void Main(string[] args)
 		{
+			EnemyMaster enemyMaster;
+			if(System.IO.File.Exists(@"D:￥enemy.json"))
+				{
+				//マスターデータファイルが存在する
+				string json=System.IO.File.ReadAllText(@"D:￥enemy.json");
+				enemyMaster=Newtonsoft.Json.JsonConvert.DeserializeObject<EnemyMaster>(json);
+			}
+			else{
+				//存在しない場合、デフォルトの値で作ってjsonを出力
+				enemyMaster=new EnemyMaster();
+				enemyMaster.Parameters=new List <EnemyParameter>();
+
+				EnemyParameter enemyParameter=new EnemyParameter();
+				enemyParameter.Name="メタルスライム";
+				enemyParameter.MaxHP=5;
+				enemyParameter.AttackPower=10;
+				enemyParameter.DefencePower=300;
+				enemyParameter.GainEXP=2000;
+
+				enemyMaster.Parameters.Add(enemyParameter);
+
+				string result = Newtonsoft.Json.JsonConvert.SerializeObject(enemyMaster);
+				System.IO.File.WriteAllText(@"D:￥enemy.json",result);
+			}
+
+			/*string json = System.IO.File.ReadAllText(@"D:￥enemy.json");
+
+			EnemyParameter parameter = Newtonsoft.Json.JsonConvert.DeserializeObject<EnemyParameter>(json);
+
+			Console.WriteLine(parameter.Name+"HP:"+parameter.MaxHP);*/
+
+
+
+
+
+			/*EnemyParameter parameter=new EnemyParameter();
+			parameter.Name="メタルスライム";
+			parameter.MaxHP=5;
+			parameter.AttackPower=10;
+			parameter.DefencePower=300;
+			parameter.GainEXP=2000;
+
+
+			string result=Newtonsoft.Json.JsonConvert.SerializeObject(parameter);
+
+			Console.WriteLine(result);
+
+			System.IO.File.WriteAllText("D:￥enemy.json",result);*/
+			//Console.ReadLine();
+
+			//return;
+
 			Player player=new Player("勇者",1,0,20,8,5);
 
 			while (true)
 			{
 				//エンカウント
-				Enemy enemy=new Enemy("スライム",7,7,3,5);
+				int enemyindex=DamageCalculator.RanbomProvider.Next(enemyMaster.Parameters.Count);
+				Enemy enemy = new Enemy(enemyMaster.Parameters[0]);
 				Battle battle= new Battle(player,enemy);
 
 				Console.WriteLine(enemy.Name+"が現れた！！");
@@ -25,11 +78,12 @@ namespace Class06
 				{
 					//ターン進行
 					Console.WriteLine(player.Name+"HP"+player.HP);
+
 					//プレイヤーの行動入力（何も受ける情報がない）
 					Console.WriteLine("Enterで1ターン進みます");
 					Console.ReadLine();
 
-					//プレイヤーと敵が慰め合う
+					//プレイヤーと敵が殴り合う
 					battleIsEnd=battle.AdvanceTurn();
 				}
 

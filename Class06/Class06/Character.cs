f9f9f1a883d0d7d3fde89810842a5ef7a26ee7a5
ゆーご <57649801+hirasawa2020@users.noreply.github.com>
2020-02-28 19:28:22 +0900

    Revert "平澤更新"
    This reverts commit f550ba9c75e08db9347ff90f37dd4e17ba46a098.

---
+++ Class06/Class06/Character.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class06
+{
+	abstract class Character
+	{
+		public string Name
+		{ get; protected set; }
+
+		public int MaxHP
+		{ get; protected set; }
+
+		public int HP
+		{ get; protected set; }
+
+		public int AttackPower
+		{ get; protected set; }
+
+		public int DefencePower
+		{ get; protected set; }
+
+
+		public bool IsAlive
+		{ get { return HP > 0; } }
+
+
+		public int Attack(Character target)
+		{
+			int damage = DamageCalculator.CalcDamage(this, target);
+
+			target.HP -= damage;
+			return damage;
+		}
+	}
+
+
+	class Enemy : Character
+	{
+		public int GainExp
+		{ get; private set; }
+
+		//敵はコンストラクタで全初期パラメータを決める
+		public Enemy(string name, int maxHP, int attackPower, int defencePower, int gainExp)
+		{
+			this.Name = name;
+			this.MaxHP = maxHP;
+			this.HP = maxHP;
+			this.AttackPower = attackPower;
+			this.DefencePower = defencePower;
+			this.GainExp = gainExp;
+		}
+	}
+
+	class Player : Character
+	{
+		public int Level
+		{ get; private set; }
+
+		public int Exp
+		{ get; private set; }
+
+		public Player(string name, int level, int exp, int maxHP, int attackPower, int defencePower)
+		{
+			this.Name = name;
+			this.Level = level;
+			this.Exp = exp;
+
+			SetParameter(maxHP, attackPower, defencePower);
+			RecoverAll();
+		}
+
+		//プレイヤーのパラメータはレベルアップによる変化を考慮して再度セットできるようにしておく
+		public void SetParameter(int maxHP, int attackPower, int defencePower)
+		{
+			this.MaxHP = maxHP;
+			this.AttackPower = attackPower;
+			this.DefencePower = defencePower;
+		}
+
+		//全回復(宿屋)
+		public void RecoverAll()
+		{
+			this.HP = MaxHP;
+		}
+	}
+
+	//ダメージ計算用クラス
+	static class DamageCalculator
+	{
+		private static Random RandomCalculator = 
+			new Random( DateTime.Now.Millisecond );
+
+		public static int CalcDamage(Character attacker, Character target)
+		{
+			//最低ダメージ＝（勇者の攻撃力-敵の守備力÷2）÷4
+			//最高ダメージ＝（勇者の攻撃力 - 敵の守備力÷2）÷2
+			int minDamage = ( attacker.AttackPower - target.DefencePower / 2 ) / 4;
+			int maxDamage = ( attacker.AttackPower - target.DefencePower / 2 ) / 2;
+
+			//最低保証１を両方に入れておく
+			if ( minDamage < 1 )
+				minDamage = 1;
+			if ( maxDamage < 1 )
+				maxDamage = 1;
+
+			int damage = RandomCalculator.Next( minDamage, maxDamage );
+
+			return damage;
+		}
+	}
+
+}
